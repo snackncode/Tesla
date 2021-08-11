9b9c58187fa30be17e6e3249f1f2dcb98a93aeb7
Ali Mahmood <69580915+snackncode@users.noreply.github.com>
2021-08-11 22:49:34 +0500

    Complete App

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+//import CarItem from './components/Caritem';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
+
+
 
 export default function App() {
-  return (
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Header />
+
+      <CarsList />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
+
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+
+
 });
